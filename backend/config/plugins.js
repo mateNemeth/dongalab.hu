4012b200e572ee359d1e8ae207fe1a569b70b1ac
mateNemeth <hello@matenemeth.hu>
2021-06-13 23:00:50 +0200

    apikey remove

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'cloudinary',
     providerOptions: {
-      cloud_name: 'matenemeth',
-      api_key: '776879884179977',
-      api_secret: 'H96gMhD-nOWM8TvRzJTMXkmpWlI',
+      cloud_name: env('CLOUD_NAME'),
+      api_key: env('API_KEY'),
+      api_secret: env('API_SECRET'),
     },
   },
   // ...
