f15a98c1a9301ddb6014ad54aa054300ec00f63e
mateNemeth <hello@matenemeth.hu>
2021-06-14 18:04:18 +0200

    swap strapi richtext editor to ckeditor5

---
+++ backend/extensions/ckeditor5/admin/src/config/ckeditor.js
@@ -0,0 +1,83 @@
+module.exports = ({
+  ////// CONFIGURATION TEMPLATE: uncomment desired lines to override default config.
+  ////// While all lines are commented, config will not change anything.
+  toolbar: {
+      items: [
+          'heading',
+          '|',
+          'bold',
+          'italic',
+          'underline',
+          'link',
+          'bulletedList',
+          'insertImage',
+          'strapiMediaLib',
+          '|',
+          'indent',
+          'outdent',
+          '|',
+          'insertTable',
+          'mediaEmbed',
+          '|',
+          'fullScreen',
+          'undo',
+          'redo',
+      ],
+      // shouldNotGroupWhenFull: true
+  },
+  image: {
+      styles: [
+        //   'alignLeft',
+        //   'alignCenter',
+        //   'alignRight',
+      ],
+      resizeOptions: [
+        //   {
+        //       name: 'resizeImage:original',
+        //       value: null,
+        //       icon: 'original'
+        //   },
+        //   {
+        //       name: 'resizeImage:50',
+        //       value: '50',
+        //       icon: 'medium'
+        //   },
+        //   {
+        //       name: 'resizeImage:75',
+        //       value: '75',
+        //       icon: 'large'
+        //   }
+      ],
+      toolbar: [
+        //   'imageStyle:alignLeft',
+        //   'imageStyle:alignCenter',
+        //   'imageStyle:alignRight',
+        //   '|',
+        //   'imageTextAlternative',
+        //   '|',
+        //   'resizeImage:50',
+        //   'resizeImage:75',
+        //   'resizeImage:original',
+        //   '|',
+        //   'linkImage',
+      ]
+  },
+  table: {
+      // contentToolbar: [
+      //     'tableColumn',
+      //     'tableRow',
+      //     'mergeTableCells',
+      //     'tableProperties',
+      //     'tableCellProperties',
+      // ]
+  },
+  heading: {
+      options: [
+          { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+          { model: 'heading3', view: 'h3', title: 'Heading', class: 'ck-heading_heading1' },
+      ]
+  },
+  htmlEmbed: {
+      // showPreviews: true
+  },
+});
