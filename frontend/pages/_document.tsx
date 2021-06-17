3a6a895437af47cfd2c5fc4796ee8fe1a4a13088
mateNemeth <hello@matenemeth.hu>
2021-06-17 17:27:07 +0200

    favicons & seo

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -20,10 +20,19 @@ class MyDocument extends Document {
             property="og:image"
             content="https://res.cloudinary.com/matenemeth/image/upload/v1623873541/large_index_min_9f93c08483.jpg"
           />
+          <meta property="og:url" content="https://dongalab.hu" />
+          <title>Dongaláb... azaz azok a csodás lábikók! :)</title>
           <meta
             name="keywords"
             content="dongaláb, achilles, ponseti, összekötött sín, gyógytorna, műtét, ortopédia, kezelés, cipő"
           />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
           <link
             rel="stylesheet"
             href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
